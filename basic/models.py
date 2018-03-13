c25fab26d218e8a77b28ecc4f6f975c4e7ea1704
Steven Omeis <2369519O@student.gla.ac.uk>
2018-03-13 16:10:44 +0000

    created models

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    id = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model:
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=264, unique=False)
+    sport =
+    host = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.name
